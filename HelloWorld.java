4aaf408393b3f722062509553f1dacf6a3b39514
КАЛОЯН СЛАВЧЕВ <88389708+KaloyanSlavchev@users.noreply.github.com>
2023-07-26 02:53:38 +0300

    removed gitignore and refactoring of HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("Hello Kaloyan and Sasho and friends!");
+        System.out.println("Hello Kaloyan !!!");
     }
 
 }
